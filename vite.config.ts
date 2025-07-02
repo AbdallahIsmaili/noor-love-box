817bc0324e1e6be2e39c9331e67a9d3e96e88fbf
AbdallahIsmaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2025-07-02 03:09:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/noor-love-box/',
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
